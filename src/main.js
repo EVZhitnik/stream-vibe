250f86f2596b3810aa8d1bd3788d06bbdcd81179
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-11-20 17:55:38 +0300

    feat: создать компонент Seasons (part 3)
    — добавление JS модуля VideoPlayer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import OverlayMenu from "@/modules/OverlayMenu";
 import Slider from "@/modules/SliderCollection";
 import TabsCollection from "./modules/TabsCollection";
+import VideoPlayerCollection from "./modules/VideoPlayerCollection";
 
 new OverlayMenu();
 new Slider();
 new TabsCollection();
+new VideoPlayerCollection();
